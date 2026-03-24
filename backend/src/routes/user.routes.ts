7ae0377ac0b1f65985879878ac6d63154edbccca
Anurag-Ranjan <quiltwcc@gmail.com>
2026-03-24 07:37:12 +0000

    add routes for user profiles and onboarding

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -3,7 +3,11 @@ import {
     loginUser,
     logoutUser,
     registerUser,
-    verifyUser
+    verifyUser,
+    onboardUser,
+    updateProfile,
+    refreshAccessToken,
+    getUserProfile
 } from '../controllers/user.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
 
@@ -13,3 +17,9 @@ userRouter.route('/signup').post(registerUser);
 userRouter.route('/verify/:token').get(verifyUser);
 userRouter.route('login').post(loginUser);
 userRouter.route('/logout').get(authMiddleware, logoutUser);
+userRouter.route('/onboard').get(authMiddleware, onboardUser);
+userRouter.route('/profile/update').get(authMiddleware, updateProfile);
+userRouter.route('/profile').get(authMiddleware, getUserProfile);
+userRouter
+    .route('/refresh-access-token')
+    .get(authMiddleware, refreshAccessToken);
