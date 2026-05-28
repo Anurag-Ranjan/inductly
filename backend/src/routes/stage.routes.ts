d49e2a345ff382353e89295e49318c50d3b2f093
Anurag-Ranjan <quiltwcc@gmail.com>
2026-05-28 22:50:21 +0530

    update stage routes to add middlewares

--- backend/src/routes/stage.routes.ts
+++ backend/src/routes/stage.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { createInductionStage, getAllStages, updateInductionStage } from "../controllers/stage.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { authorizeClubRole } from "../middlewares/role.middleware";
 
 export const stageRouter = Router();
 
-stageRouter.get("/", getAllStages);
-stageRouter.post("/", createInductionStage);
-stageRouter.patch("/:stageId", updateInductionStage);
+stageRouter.get("/",authMiddleware, getAllStages);
+stageRouter.post("/",authMiddleware, authorizeClubRole, createInductionStage);
+stageRouter.patch("/:stageId",authMiddleware, authorizeClubRole, updateInductionStage);
